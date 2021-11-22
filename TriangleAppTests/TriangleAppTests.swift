1da23b814c0628cd34ad126c304141a0bcb7ade3
Saputra <agungdwisaputra0808@gmail.com>
2021-11-22 10:32:48 +0700

    init commit

--- TriangleAppTests/TriangleAppTests.swift
+++ TriangleAppTests/TriangleAppTests.swift
@@ -29,5 +29,51 @@ class TriangleAppTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
+    
+    func testInvalidInputSides() {
+        XCTAssertThrowsError(try detectTriangle(-1, 2, 3)) { error in
+            XCTAssertEqual(error as? TriangleError, TriangleError.invalidInput)
+        }
+    }
+    
+    func testDetectEquilateralTriangle() {
+        XCTAssertEqual(try detectTriangle(2, 2, 2), "Segitiga Sama Sisi")
+    }
+    
+    func testDetectIsoscelesTriangle() {
+      XCTAssertEqual(try detectTriangle(1, 2, 2), "Segitiga Sama Kaki")
+    }
+    
+    func testRandomTriangle() {
+        XCTAssertEqual(try detectTriangle(1, 2, 3), "Segitiga Sembarang")
+    }
+    
+    func detectTriangle(
+        _ sideA: Int,
+        _ sideB: Int,
+        _ sideC: Int
+    ) throws -> String {
+        let sides = [sideA, sideB, sideC].sorted()
+        for side in sides {
+            if side < 1 {
+                throw TriangleError.invalidInput
+            }
+            
+        }
+        
+        if sides[0] == sides[1] && sides[0] == sides[2] {
+            return "Segitiga Sama Sisi"
+        } else if sides[0] == sides[1] || sides[1] == sides[2] {
+            return "Segitiga Sama Kaki"
+        }
+        else {
+            return "Segitiga Sembarang"
+        }
+        
+    }
 
+}
+
+enum TriangleError: Error {
+    case invalidInput
 }
